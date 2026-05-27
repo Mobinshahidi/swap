c24177421c262ab2818d1d0069a276d3dcf26ccb
Mobinshahidi <mowbinsh@gmail.com>
2026-05-27 14:14:04 +0330

    Add GitHub links and bump version to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.swap.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
